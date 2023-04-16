0f44d35bd9de0c82d60f9fa6e5cd256423120dca
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-16 11:08:29 +0200

    Add user preference to set default mode

--- assets/js/templates/darkLightMode.js
+++ assets/js/templates/darkLightMode.js
@@ -1,10 +1,14 @@
 // Kevin Powells' How to make a website light/dark toggle with CSS & JS https://www.youtube.com/watch?v=wodWDIdV9BY&t=131s
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const userPreferenceDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches;
 
 var darkMode = localStorage.getItem('darkMode');
 
-if (darkMode == 'enabled') {
-  enableDarkMode()
+if (
+  (userPreferenceDarkMode && !darkMode) ||
+  darkMode == 'enabled'
+) {
+  enableDarkMode();
 }
 
 /*
